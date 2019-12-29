020cb8041341b829ba3e99edd4c105e8d181fe97
Yeltsin Ramirez <yeltsin.ramirez@emqu.net>
2019-12-29 18:48:25 -0400

    2do config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-
+import exphbs from 'express-handlebars';
+import path from 'path';
 const app = express();
 
 
-app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
+app.set('views',path.join(__dirname, 'views'));
+app.set('.hbs', exphbs({
+    extname:'.hbs',
+    layoutsDir:path.join(app.get('views'),'layouts'),
+}));
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 
 app.listen(app.get('port'), ()=> 
 console.log(`Server on port ${app.get('port')}`));
