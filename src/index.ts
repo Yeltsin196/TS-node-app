c00e3ba34e9b7e43e8d3c2cb66821e6c5fe21c1e
Yeltsin Ramirez <yeltsin.ramirez@emqu.net>
2019-12-29 18:54:34 -0400

    3ero config

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,13 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views',path.join(__dirname, 'views'));
-app.set('.hbs', exphbs({
+app.engine('.hbs', exphbs({
     extname:'.hbs',
     layoutsDir:path.join(app.get('views'),'layouts'),
+    partialsDir:path.join(app.get('views'),'partials'),
+    helpers:require('./lib/helpers'),
 }));
+app.set('view engine','.hbs');
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
